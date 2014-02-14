6832508fe629cec04ee6d05b3be7d697765d8e4f
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2014-02-14 08:57:14 +0100

    Add Hello project

---
+++ Hello.cmake
@@ -0,0 +1,5 @@
+
+set(HELLO_REPO_URL https://github.com/BlueBrain/Hello.git)
+set(HELLO_DEPENDS bluebrain REQUIRED Boost)
+set(HELLO_BOOST_COMPONENTS "unit_test_framework program_options")
+set(HELLO_DEB_DEPENDS libboost-test-dev libboost-program-options-dev)
