08548d56acfce9f967a2bc32ee46f9fc8e76e07b
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2014-05-06 08:49:41 +0200

    HPX config

---
+++ hpx.cmake
@@ -0,0 +1,12 @@
+set(HPX_PACKAGE_VERSION 0.9)
+set(HPX_DEPENDS papi tau apex sodium REQUIRED Boost hwloc HDF5)
+set(HPX_BOOST_COMPONENTS "date_time filesystem program_options regex serialization system thread atomic chrono")
+set(HPX_DEB_DEPENDS libboost-date-time-dev libboost-filesystem-dev
+  libboost-program-options-dev libboost-regex-dev libboost-serialization-dev
+  libboost-system-dev libboost-thread-dev tau libpapi-dev
+  libgoogle-perftools-dev libunwind8-dev)
+set(HPX_OPTIONAL ON)
+set(HPX_FORCE_BUILD ${CI_BUILD})
+if(NOT WIN32)
+  set(HPX_REPO_URL https://github.com/STEllAR-GROUP/hpx.git)
+endif()
