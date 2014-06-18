b411a4fda9347517376a152fecb9bccaee9152cd
Daniel Nachbaur <daniel.nachbaur@epfl.ch>
2014-06-18 14:43:27 +0200

    Use existing dash version

--- dash.cmake
+++ dash.cmake
@@ -1,6 +1,6 @@
-set(DASH_PACKAGE_VERSION 1.1.0)
+set(DASH_PACKAGE_VERSION 1.0.0)
 set(DASH_DEPENDS bluebrain REQUIRED Lunchbox Boost)
 set(DASH_BOOST_COMPONENTS serialization)
 set(DASH_DEB_DEPENDS libboost-serialization-dev)
 set(DASH_REPO_URL https://github.com/BlueBrain/dash.git)
-set(DASH_REPO_TAG 1.1)
+set(DASH_REPO_TAG 1.0)
