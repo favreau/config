21cfd3aa00ad1aa6261c286ad194de63cc9a2d9d
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2014-07-16 10:55:41 +0200

    Add lunchbox as dependency

--- EDA1.cmake
+++ EDA1.cmake
@@ -1,6 +1,6 @@
 
 set(EDA1_REPO_URL https://github.com/HBPVIS/EDA1.git)
-set(EDA1_DEPENDS REQUIRED libzmq flatbuffers Boost)
+set(EDA1_DEPENDS REQUIRED libzmq flatbuffers Boost Lunchbox)
 set(EDA1_OPTIONAL ON)
 set(EDA1_BOOST_COMPONENTS "unit_test_framework program_options")
 set(EDA1_DEB_DEPENDS libboost-test-dev libboost-program-options-dev)
