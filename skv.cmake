464ab54c5f3df0c3ae692db0f54dd14548843073
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2014-09-08 11:11:18 +0200

    Rename SKV repo

--- skv.cmake
+++ skv.cmake
@@ -1,5 +1,5 @@
 
-set(SKV_REPO_URL https://github.com/BlueBrain/code.git)
+set(SKV_REPO_URL https://github.com/BlueBrain/skv.git)
 set(SKV_DEPENDS REQUIRED OFED MPI)
 set(SKV_OPTIONAL ON)
 set(SKV_CMAKE_INCLUDE "SYSTEM")
