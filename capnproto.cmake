2a19794c66c1cb10be90d511dd7a91789eaac3c0
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2014-11-12 14:42:09 +0100

    Move stuff to config.extra

--- capnproto.cmake
+++
@@ -1,9 +0,0 @@
-set(CAPNPROTO_REPO_URL https://github.com/kentonv/capnproto.git)
-set(CAPNPROTO_OPTIONAL ON)
-set(CAPNPROTO_SOURCE "${CMAKE_SOURCE_DIR}/src/capnproto")
-set(CAPNPROTO_CMAKE_INCLUDE "SYSTEM")
-set(CAPNPROTO_EXTRA
-  CONFIGURE_COMMAND cd ${CAPNPROTO_SOURCE}/c++ && ./setup-autotools.sh && autoreconf -i && ./configure "--prefix=${CMAKE_CURRENT_BINARY_DIR}/install"
-  BUILD_COMMAND cd ${CAPNPROTO_SOURCE}/c++ && make -j8
-  INSTALL_COMMAND cd ${CAPNPROTO_SOURCE}/c++ && make install
-)
