e4473c4c1bb53299727ba52777e9d995da9806f5
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2014-11-26 11:26:56 +0100

    Move music config

--- music.cmake
+++
@@ -1,7 +0,0 @@
-set(MUSIC_PACKAGE_VERSION 1.0)
-set(MUSIC_OPTIONAL ON)
-set(MUSIC_REPO_URL https://github.com/BlueBrain/music.git)
-set(MUSIC_REPO_TAG bbp)
-set(MUSIC_DEPENDS REQUIRED MPI
-                  OPTIONAL OpenGL GLUT PythonInterp PythonLibs Threads)
-set(MUSIC_FORCE_BUILD ${CI_BUILD})
