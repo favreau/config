47180a3c0b559dd35ec4cb26c067d0e19badf684
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2015-04-13 14:34:02 +0200

    Remove obsolete dependency

--- Livre.cmake
+++ Livre.cmake
@@ -3,8 +3,8 @@ if(WIN32)
 endif()
 set(LIVRE_PACKAGE_VERSION 0.2.0)
 set(LIVRE_REPO_URL ssh://bbpcode.epfl.ch/viz/Livre)
-set(LIVRE_DEPENDS BBPTestData FLATIndex OpenMP Tuvok VTune GLEW_MX zeq
-  FlatBuffers RESTConnector LibJpegTurbo
+set(LIVRE_DEPENDS BBPTestData OpenMP Tuvok VTune GLEW_MX zeq FlatBuffers
+  RESTConnector LibJpegTurbo
   REQUIRED Boost Collage dash Equalizer Lunchbox OpenGL PNG Qt4 Threads)
 set(LIVRE_BOOST_COMPONENTS
   "filesystem program_options thread system regex unit_test_framework")
