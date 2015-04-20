c96c7da8079d9b92306d045bb8a5df970c8cc0f2
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2015-04-20 13:14:44 +0200

    Fix Livre URI

--- Livre.cmake
+++ Livre.cmake
@@ -2,7 +2,7 @@ if(WIN32)
   return()
 endif()
 set(LIVRE_PACKAGE_VERSION 0.2.0)
-set(LIVRE_REPO_URL ssh://bbpcode.epfl.ch/viz/Livre)
+set(LIVRE_REPO_URL https://github.com/BlueBrain/Livre)
 set(LIVRE_DEPENDS BBPTestData OpenMP Tuvok VTune GLEW_MX zeq FlatBuffers
   RESTConnector LibJpegTurbo
   REQUIRED Boost Collage dash Equalizer Lunchbox OpenGL PNG Qt4 Threads)
