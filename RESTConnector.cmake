7c66e0f8d310348540a2385c5faf5ba88be06d3d
Cyrille Favreau <cyrille.favreau@epfl.ch>
2015-05-13 13:59:18 +0200

    BVWS-219: Change RESTConnector name to RESTBridge

--- RESTConnector.cmake
+++
@@ -1,6 +0,0 @@
-set(RESTCONNECTOR_VERSION 0.1)
-set(RESTCONNECTOR_REPO_URL https://github.com/BlueBrain/RESTConnector.git)
-set(RESTCONNECTOR_DEPENDS REQUIRED Boost cppnetlib Lunchbox zeq FlatBuffers
-                          OPTIONAL bluebrain)
-set(RESTCONNECTOR_BOOST_COMPONENTS "unit_test_framework program_options system thread")
-set(RESTCONNECTOR_DEB_DEPENDS libboost-test-dev libboost-program-options-dev libboost-system-dev libboost-thread-dev)
